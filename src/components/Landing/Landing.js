04f2703edb405a493d6b11c23436a3e33e9664a3
justinalize <justinalize@gmail.com>
2021-01-05 19:48:32 -0500

    converted wireframe to react and did route

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,9 +1,13 @@
 import React from "react"
+import Content from './Content'
+import { Route, Switch } from 'react-router-dom'
+import styled from 'styled-components'
 
 const Landing = () => {
     return(
-        <>
-        </>
+      <div>
+        <Content />
+      </div>
     )
 }
 
